93c3644fec4eee0d9eb1514387c6fdc269747154
Fatima <fati27khan@gmail.com>
2025-03-02 01:55:00 -0800

    Add files via upload

---
+++ Lab1.py
@@ -0,0 +1,92 @@
+               # Program For Comments 
+x=1
+#The initial value of x is 1
+if x>0 :
+    print("These are two comments") #Print a string
+               #Program for taking a input
+txt=input("Type something to text this out: ")
+print(txt)
+               #Program for multiple statements on a single line
+print("statement1")
+print("statemenr2")
+print("statement1");print("statemenr2")
+               #Program for space indentation
+x=1
+if x>0 :
+ print("This statement has a single space indentation")
+ print("This statement has a single space indentation")
+               #Program for tab indentation
+if x>0 :
+    print("This statement has a single tab indentation")
+    print("This statement has a single tab indentation")
+               #Program for single space + tab indentation
+if x>0 :
+     print("This statement has a single space+tab indentation")
+     print("This statement has a single space+tab indentation")
+               #Program for finding a datatype
+a=1452
+print (type(a))
+b=(-4587)
+print (type(b))
+c=0
+print (type(c))
+g=1.03
+print (type(g))
+h=-11.23
+print (type(h))
+i=.34
+print (type(i))
+j=2.12e-10
+print (type(j))
+k=5E220
+print (type(a))
+              #Program for Complex datatype
+x=complex(1,2)
+print (type(x))
+z=1+2j
+print (type(z))
+z=1+2J
+print (type(z))
+             #Program for boolean datatype
+x=True
+print (type(x))
+y=False
+print (type(y))
+             #Program for string datatype
+str1="String"
+print (type(str1))
+str2='Day"s'
+print (str2)
+str3="Day's"
+print (str3)
+            #Program for special character
+print("This is a backslash (\\) mark.")
+print("This is a tab \t key.")
+print("These are \'single quotes\'")
+print("These are \"double quotes\"")
+print("This is a new line \nNew line")
+            #Program for accessing string elements
+String1="Python Tutorial"
+print(String1[0])    #Print first character
+print(String1[-15])  #Print first character
+print(String1[14])   #Print last character
+print(String1[-1])   #Print last character
+print(String1[4])    #Print 4th character
+             #Program for list
+my_list1=[5,12,13,14]
+print(my_list1)
+my_list2=['red','green','blue','white']
+print(my_list2)
+my_list3=['red',12,112.12]
+print(my_list3)
+            #Program for displaying different list indices
+color_list=["Red","Green","Blue","White"]
+print(color_list[0])
+print(color_list[0],color_list[3])
+print(color_list[2])
+            #Program for list slicing
+color_list1=["Orange","Yellow","Gray"]
+print(color_list1[0:3])
+print(color_list1[-3:-1])
+
+             
